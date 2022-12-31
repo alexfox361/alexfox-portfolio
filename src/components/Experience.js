399a4d09c0a934e6341577eded022186841b358c
alexfox361 <71406323+alexfox361@users.noreply.github.com>
2022-12-31 09:25:56 -0800

    updated

---
+++ src/components/Experience.js
@@ -0,0 +1,21 @@
+import { AcademicCapIcon } from "@heroicons/react/solid";
+import React from "react";
+
+{/* <AcademicCapIcon class="w-10 inline-block mb-4" /> */}
+
+export default function Experience() {
+  return (
+    <section id="exp" class="text-white bg-gray-800 body-font">
+      <div class="container px-10 py-10 mx-auto">
+        <div class="mb-10">
+          <h1 class="text-center md:text-5xl text-2xl mb-4">
+            Experience
+          </h1>
+          <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+            From my education, personal projects, and jobs, I've learned these skills:
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <header className="border-b bg-black  md:sticky top-0 z-10 border-white">
+    <header class="border-b-2 bg-gray-900 md:sticky top-0 z-10 border-purple-400">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="text-primary title-font font-medium mb-4 md:mb-0">
           <a href="#about" className="text-purple-400 sm:text-3xl text-2xl mb-1 font-medium title-font ml-3">
@@ -12,6 +12,9 @@ export default function Navbar() {
           </a>
         </a>
         <nav className="md:mr-auto text-white md:ml-4 md:py-1 md:pl-4 md:border-l md:border-white	flex flex-wrap items-center text-base justify-center">
+          <a href="#workexp" className="mr-5 hover:text-purple-400">
+            Experience
+          </a>
           <a href="#projects" className="mr-5 hover:text-purple-400">
             Projects
           </a>
@@ -25,8 +28,8 @@ export default function Navbar() {
         <a
           href="#contact"
           className="inline-flex mt-2">
-          <SocialIcon url="https://www.linkedin.com/in/alex-fox-996364212" fgColor="white"/>
-          <SocialIcon url="https://github.com/alexfox361" className="ml-5" fgColor="white"/>
+          <SocialIcon url="https://www.linkedin.com/in/alex-fox-996364212"  target="_blank" fgColor="white"/>
+          <SocialIcon url="https://github.com/alexfox361" className="ml-5"  target="_blank" fgColor="white"/>
 
           {/* <Icon icon="mdi-light:home" /> */}
         </a>
